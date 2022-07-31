a384044abbd300b0b73d0f2a8d0e34a80ea63d4f
huseyinturksoy <102436025+huseyinturksoy@users.noreply.github.com>
2022-07-31 22:16:35 +0300

    ht

--- src/FeatureTimur/Test01.java
+++ src/FeatureTimur/Test01.java
@@ -5,6 +5,7 @@ import jdk.swing.interop.SwingInterOpUtils;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("hello from Timur");
+        System.out.println("hello from huseyin");
     }
 
 
