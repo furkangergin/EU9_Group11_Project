4a03a2b732f7fc68528f8c05dc59fb80500d5c9a
omer dogruyol <new@email.com>
2022-07-31 22:20:53 +0300

    finished my awesome job

---
+++ src/featureOmerDogruyol/TestOmerDogruyol.java
@@ -0,0 +1,7 @@
+package featureOmerDogruyol;
+
+public class TestOmerDogruyol {
+    public static void main(String[] args) {
+        System.out.println("I added some good features our project");
+    }
+}
