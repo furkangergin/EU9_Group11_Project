7a02fc6ad5f8341894427f0444f29d0e66d7840d
Timur Guclu <tmrguclu@hotmail.com>
2022-07-31 22:07:40 +0300

    Create test01

--- src/FeatureTimur/Test01.java
+++ src/FeatureTimur/Test01.java
@@ -1,5 +1,13 @@
 package FeatureTimur;
 
-public class Test01 {
+import jdk.swing.interop.SwingInterOpUtils;
 
-}
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("hello from Timur");
+    }
+
+
+    }
+
+
